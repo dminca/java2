bc09a8c3d7be135fa0a7c5470fbbc76a35beb92f
completit <student@student-PC>
2014-11-19 18:36:01 +0200

    Client setup

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,12 +6,27 @@
 
 package client;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  *
  * @author student
  */
 public class Client {
     public static void main(String [] args){
-        
+        try{
+            Socket socket = new Socket("", 4321);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            out.println("HELLO WORLD!");
+            String line = in.readLine();
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
